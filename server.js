53a5ad0638135dc1b9c57d5b157900aa3e4a0703
trble91 <daleyaaron@gmail.com>
2023-04-13 14:56:56 -0400

    deploy ready

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3000
 
 app.use("/", routes);
 
-db.connection.on("connected", () => {
+db.on("connected", () => {
   console.clear();
   console.log("Connected to MongoDB!");
 
